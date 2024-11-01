9f42d08a34e43e547f1dc107cd14165a558aa063
Lucas Smaniotto <101435037+lucassmaniotto@users.noreply.github.com>
2024-11-01 01:04:37 +0000

    feat: implementacao parser

--- Trabalho Final/Lexer.hs
+++ Trabalho Final/Lexer.hs
@@ -1,5 +1,7 @@
 module Lexer where
 
+import Data.Char
+
 data Expr = BTrue
           | BFalse
           | Num Int
@@ -11,3 +13,35 @@ data Expr = BTrue
 data Ty = TBool
         | TNum
         deriving (Show, Eq)
+
+data Token = TokenTrue
+           | TokenFalse
+           | TokenNum Int
+           | TokenAdd
+           | TokenAnd
+           | TokenIf
+           | TokenThen
+           | TokenElse
+           deriving Show
+
+lexer :: String -> [Token]
+lexer [] = []
+lexer ('+':cs) = TokenAdd : lexer cs
+lexer (c:cs) 
+      | isSpace c = lexer cs
+      | isAlpha c = lexerKW (c:cs)
+      | isDigit c = lexerNum (c:cs)
+
+lexerNum :: String -> [Token]
+lexerNum cs = case span isDigit cs of
+                (num, rest) -> TokenNum (read num) : lexer rest
+
+lexerKW :: String -> [Token]
+lexerKW cs = case span isAlpha cs of
+                ("true", rest) -> TokenTrue : lexer rest
+                ("false", rest) -> TokenFalse : lexer rest
+                ("if", rest) -> TokenIf : lexer rest
+                ("then", rest) -> TokenThen : lexer rest
+                ("else", rest) -> TokenElse : lexer rest
+                ("and", rest) -> TokenAnd : lexer rest
+                
